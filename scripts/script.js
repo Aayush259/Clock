5230b2be4d149d5cdae59998c098b9e1b2aed110
Aayush259 <aayushk4980@gmail.com>
2024-04-21 16:56:47 +0530

    Added Timer Countdown Functionality

--- scripts/script.js
+++ scripts/script.js
@@ -106,6 +106,108 @@ const StartStopWatch = () => {
 
 }
 
+/*
+    This function starts the timer if everything is correct
+*/
+const StartTimer = () => {
+
+    // Getting all input fields for hour, minute and second.
+    const InputHour = document.getElementById('hour-input');
+    const InputMinute = document.getElementById('minute-input');
+    const InputSecond = document.getElementById('second-input');
+
+    // Converting the string value of input fields into integer and storing in their respective variables.
+    let HourTimer = parseInt(InputHour.value);
+    let MinuteTimer = parseInt(InputMinute.value);
+    let SecondTimer = parseInt(InputSecond.value);
+
+    // Getting start and stop button.
+    const StartButton = document.getElementById('timer-start-button');
+    const StopButton = document.getElementById('timer-stop-button');
+
+    /*
+        If HourTimer and MinuteTimer and SecondTimer, all are not a valid integer at same time, then return from this function.
+        If their values are zero or negative then return from this function. 
+    */
+    if (isNaN(HourTimer) && isNaN(MinuteTimer) && isNaN(SecondTimer)) {
+        return;
+    } else if (HourTimer <= 0 || SecondTimer <= 0 || MinuteTimer <= 0) {
+        return;
+    }
+    
+    // If everything is fine then hide start button and display stop button.
+    StartButton.classList.add('none-display');
+    StopButton.classList.remove('none-display');
+
+    // If HourTimer, MinuteTimer or SecondTimer isNaN then make them zero.
+    HourTimer = (isNaN(HourTimer)) ? 0 : HourTimer;
+    MinuteTimer = (isNaN(MinuteTimer)) ? 0 : MinuteTimer;
+    SecondTimer = (isNaN(SecondTimer)) ? 0 : SecondTimer;
+
+    // Getting Hour, Minute, Second elements which will show countdown.
+    const Hour = document.getElementById('hour-countdown');
+    const Minute = document.getElementById('minute-countdown');
+    const Second = document.getElementById('second-countdown');
+
+    // If second count is greater than 60, then adjust it into minute.
+    while (true) {
+        if (SecondTimer >= 60) {
+            SecondTimer -= 60;
+            MinuteTimer += 1;
+        } else {
+            break;
+        }
+    }
+
+    // If minute count is greater than 60, then adjust it into hour.
+    while (true) {
+        if (MinuteTimer >= 60) {
+            MinuteTimer -= 60;
+            HourTimer += 1;
+        } else {
+            break;
+        }
+    }
+
+    // Initializing countdown of Hour, Minute and Second elements.
+    Hour.textContent = HourTimer.toString().padStart(2, '0');
+    Minute.textContent = MinuteTimer.toString().padStart(2, '0');
+    Second.textContent = SecondTimer.toString().padStart(2, '0');
+
+    // Starting countdown using setInterval.
+    let SecondInterval = setInterval(() => {
+        
+        // Decreasing SecondTimer by 1.
+        SecondTimer = SecondTimer - 1;
+
+        /*
+            If SecondTimer is smaller than zero and MinuteTimer is greater than 0, then set SecondTimer to 59 and Decrease MinuteTimer by one.
+            If SecondTimer, MinuteTimer and HourTimer are smaller than 1 then countdown is finished, so hide stop button and show start button.
+        */
+        if ((SecondTimer < 0) && (MinuteTimer >= 1)) {
+            SecondTimer = 59;
+            MinuteTimer -= 1;
+        } else if ((SecondTimer <= 0) && (MinuteTimer < 1) && (HourTimer < 1)) {
+            StopButton.classList.add('none-display');
+            StartButton.classList.remove('none-display');
+            clearInterval(SecondInterval);
+        }
+
+        // If SecondTimer is smaller than 0 or MinuteTimer is smaller than 0 and HourTimer is greater than 0 the reset MinuteTimer and SecondTimer to 59 and decrease HourTimer by 1.
+        if (((SecondTimer < 0) || (MinuteTimer < 0)) && (HourTimer >= 1)) {
+            MinuteTimer = 59;
+            SecondTimer = 59;
+            HourTimer -= 1;
+        }
+
+        // Updating the Hour, Minute and Second countdown on screen every second.
+        Hour.textContent = HourTimer.toString().padStart(2, '0');
+        Minute.textContent = MinuteTimer.toString().padStart(2, '0');
+        Second.textContent = SecondTimer.toString().padStart(2, '0');
+        
+    }, 1000)
+}
+
 function activateClock() {
     ClockContainer.innerHTML = `
     <h2 class="time">
@@ -164,6 +266,48 @@ function activateStopWatch() {
     })
 }
 
+function activateTimer() {
+    
+    ClockContainer.innerHTML = `
+    <div class="time-input flex">
+
+        <div class="hour-input-container flex">
+            <label for="hour-input" class="timer-label">Hour</label>
+            <input type="number" min="1" name="hour" id="hour-input" placeholder="0" class="timer-input">
+        </div>
+
+        <div class="minute-input-container flex">
+            <label for="minute-input" class="timer-label">Minute</label>
+            <input type="number" min="1" name="minute" id="minute-input" placeholder="0" class="timer-input">
+        </div>
+
+        <div class="second-input-container flex">
+            <label for="second-input" class="timer-label">Second</label>
+            <input type="number" min="1" name="second" id="second-input" placeholder="0" class="timer-input">
+        </div>
+
+    </div>
+
+    <button id="timer-start-button">Start</button>
+    <button id="timer-stop-button" class="none-display">Stop</button>
+
+    <div class="timer-output-container time">
+        <span id="hour-countdown">00</span> :
+        <span id="minute-countdown">00</span> :
+        <span id="second-countdown">00</span>
+    </div>
+    `
+
+    const StartButton = document.getElementById('timer-start-button');
+
+    StartButton.addEventListener('click', () => {
+        
+        StartTimer();
+        
+    })
+
+}
+
 // Activate clock at starting.
 activateClock();
 
@@ -171,4 +315,4 @@ Clock.addEventListener('click', activateClock);
 
 StopWatch.addEventListener('click', activateStopWatch)
 
-Timer.addEventListener('click', () => {console.log('Timer')});
+Timer.addEventListener('click', activateTimer);
